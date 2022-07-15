1fc131753f27b00a4a2240c46a4cf2567ea81fac
bittoMazza <robertomazza99@gmail.com>
2022-07-15 16:56:35 +0200

    completed buy-comics container

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -4,10 +4,10 @@
             <h2 class="text-white fs-5"> -- > CONTENT GOES HERE   -- </h2>
         </div>
         <div class="buy-comics">
-            <div class="container d-flex justify-content-between">
+            <div class="container d-flex justify-content-between align-items-center">
                 <div v-for="(buyLink,index) in buyComics" :key="index">
-                    <img class="logos-buy-comics" :src="buyLink.url" alt="">
-                    <span> {{ buyLink.text }}</span>
+                    <img class="logos-buy-comics" :src="buyLink.urlImg" alt="">
+                    <span class="text-white"> {{ buyLink.text }}</span>
                 </div>
             </div>           
         </div>
@@ -21,23 +21,23 @@ export default {
             buyComics:[
                 {
                     text:'DIGITAL COMICS',
-                    url:'../assets/img/buy-comics-digital-comics.png',
+                    urlImg:'http://localhost:8080/img/buy-comics-digital-comics.png',
                 },
-                 {
+                {
                     text:'DC MERCHANDISE',
-                    url:'../assets/img/buy-comics-digital-comics.png'
+                    urlImg:'http://localhost:8080/img/buy-comics-merchandise.png',
                 },
-                 {
+                {
                     text:'SUBSCRIPTION',
-                    url:'../assets/img/buy-comics-digital-comics.png'
+                    urlImg:'http://localhost:8080/img/buy-comics-shop-locator.png',
                 },
-                 {
+                {
                     text:'COMIC SHOP LOCATOR',
-                    url:'../assets/img/buy-comics-digital-comics.png'
+                    urlImg:'http://localhost:8080/img/buy-comics-subscriptions.png',
                 },
-                 {
+                {
                     text:'DC POWER VISA',
-                    url:''
+                    urlImg:'http://localhost:8080/img/buy-dc-power-visa.svg',
                 },
             ]
         }
@@ -53,9 +53,11 @@ export default {
         background-color: black;
     }
     .buy-comics{
+        padding: 50px 0;
         background-color: $mainColor;
     }
     .logos-buy-comics{
-        width: 30px;
+        height: 50px;
+        margin-right: 10px;
     }
 </style>
