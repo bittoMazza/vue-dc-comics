342abebd3640a68d4c67cd418654f44fd7cab3af
bittoMazza <robertomazza99@gmail.com>
2022-07-17 14:19:28 +0200

    created buyComics components

---
+++ src/components/BuyComics.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="buy-comics">
+            <div class="container d-flex justify-content-between align-items-center px-5">
+                <div v-for="(buyLink,index) in buyComics" :key="index">
+                    <img class="logos-buy-comics" :src="buyLink.urlImg" alt="">
+                    <a href="#" class="text-white text-decoration-none"> {{ buyLink.text }}</a>
+                </div>
+            </div>           
+        </div>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return{
+            buyComics:[
+                {
+                    text:'DIGITAL COMICS',
+                    urlImg:'http://localhost:8080/img/buy-comics-digital-comics.png',
+                },
+                {
+                    text:'DC MERCHANDISE',
+                    urlImg:'http://localhost:8080/img/buy-comics-merchandise.png',
+                },
+                {
+                    text:'SUBSCRIPTION',
+                    urlImg:'http://localhost:8080/img/buy-comics-shop-locator.png',
+                },
+                {
+                    text:'COMIC SHOP LOCATOR',
+                    urlImg:'http://localhost:8080/img/buy-comics-subscriptions.png',
+                },
+                {
+                    text:'DC POWER VISA',
+                    urlImg:'http://localhost:8080/img/buy-dc-power-visa.svg',
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/variable.scss";
+     .buy-comics{
+        padding: 50px 0;
+        background-color: $mainColor;
+    }
+    .logos-buy-comics{
+        height: 40px;
+        margin-right: 10px;
+    }
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,44 +3,15 @@
         <div class="content-container container py-5">
             <h2 class="text-white fs-5"> -- > CONTENT GOES HERE   -- </h2>
         </div>
-        <div class="buy-comics">
-            <div class="container d-flex justify-content-between align-items-center px-5">
-                <div v-for="(buyLink,index) in buyComics" :key="index">
-                    <img class="logos-buy-comics" :src="buyLink.urlImg" alt="">
-                    <a href="#" class="text-white text-decoration-none"> {{ buyLink.text }}</a>
-                </div>
-            </div>           
-        </div>
+        <BuyComics/>
     </main>
 </template>
 
 <script>
+import BuyComics from "./BuyComics.vue";
 export default {
-    data: function(){
-        return{
-            buyComics:[
-                {
-                    text:'DIGITAL COMICS',
-                    urlImg:'http://localhost:8080/img/buy-comics-digital-comics.png',
-                },
-                {
-                    text:'DC MERCHANDISE',
-                    urlImg:'http://localhost:8080/img/buy-comics-merchandise.png',
-                },
-                {
-                    text:'SUBSCRIPTION',
-                    urlImg:'http://localhost:8080/img/buy-comics-shop-locator.png',
-                },
-                {
-                    text:'COMIC SHOP LOCATOR',
-                    urlImg:'http://localhost:8080/img/buy-comics-subscriptions.png',
-                },
-                {
-                    text:'DC POWER VISA',
-                    urlImg:'http://localhost:8080/img/buy-dc-power-visa.svg',
-                },
-            ]
-        }
+    components:{
+        BuyComics,
     }
 }
 </script>
@@ -51,13 +22,5 @@ export default {
         position: relative;
         z-index: 1;
         background-color: black;
-    }
-    .buy-comics{
-        padding: 50px 0;
-        background-color: $mainColor;
-    }
-    .logos-buy-comics{
-        height: 40px;
-        margin-right: 10px;
     }
 </style>
